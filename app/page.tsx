7c7f85272f7281cf3b55cc8e1c01e17ef43d81d4
PunitDev0 <punitdeveloper1@tgmail.com>
2024-09-20 10:39:43 +0530

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,35 @@
+"use client"
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import NavbarComponent from "@/components/navbar";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
+import { useState } from "react";
 
 
 export default function Home() {
+  const [nav, setnav] = useState(false)
+  window.onscroll = () => {
+    if (window.scrollY > 237) {
+      setnav(false)
+    } else {
+      setnav(true)
+
+    }
+
+    console.log(window.scrollY);
+  }
+
+
   return(
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto ">
       <div className="max-w-7xl w-full">
+        
+        {nav && <NavbarComponent/>}
         <FloatingNav navItems={navItems}/>               
         <Hero/>
         <Grid/>
