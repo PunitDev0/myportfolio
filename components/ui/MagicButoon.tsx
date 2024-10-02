a043c77380d7b15542b77e7704808285559ec5eb
PunitDev0 <punitdeveloper1@tgmail.com>
2024-10-03 02:17:39 +0530

    fixed my show my work button error

--- components/ui/MagicButoon.tsx
+++ components/ui/MagicButoon.tsx
@@ -1,23 +1,41 @@
-"use client"
-import React from 'react'
-import Link from 'next/link';
-const MagicButoon = ({
-    title,  icon, position, handleClick, OtherClases
-}:{
-    title: string; icon: React.ReactNode, position:string; handleClick?: () => void; OtherClases?:string;
+"use client";
+import React from 'react';
+
+const MagicButton = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  OtherClasses,
+}: {
+  title: string;
+  icon: React.ReactNode;
+  position: string;
+  handleClick?: () => void;
+  OtherClasses?: string;
 }) => {
+  const handleRedirect = () => {
+    if (handleClick) {
+      handleClick();
+    }
+    window.open("https://github.com/PunitDev0", "_blank"); // Opens the link in a new tab
+  };
+
   return (
-    <Link href={"https://github.com/PunitDev0"}>
-          <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none md-w-60 md:mt-10">
+    <button
+      className={`relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10 ${OtherClasses}`}
+      onClick={handleRedirect}
+    >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${OtherClases}`}>
+      <span
+        className={`inline-flex h-full w-full items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2`}
+      >
         {position === 'left' && icon}
         {title}
         {position === 'right' && icon}
       </span>
     </button>
-    </Link>
-  )
-}
+  );
+};
 
-export default MagicButoon
+export default MagicButton;
